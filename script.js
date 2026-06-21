04dba0fa08cb9ccfa6d1be21203718730f3e35f6
rafiqulislamraj58-design <rafiqulislamraj58@gmail.com>
2026-06-21 13:01:04 +0600

    event capturing and bubling  add  nightmode fixed

--- script.js
+++ script.js
@@ -5,12 +5,13 @@
  let hadingcolor = document.querySelector('#takflow')
 
  toggole.addEventListener('click',()=>{
-    if(document.body.style.backgroundColor==='black'){
-        document.body.style.backgroundColor='white';
-        hadingcolor.style.color = 'black';
+    const currentTheme = document.body.getAttribute('data-theme');
+    if(currentTheme==='light'){
+       document.body.setAttribute('data-theme', 'dark');
+        toggole.innerText = '☀️';
     }else{
-       document.body.style.backgroundColor = 'black';
-        hadingcolor.style.color = 'white';
+       document.body.setAttribute('data-theme', 'light');
+        toggole.innerText = '🌙';
     }
  })
 const taskList = document.querySelector('#taskList')
@@ -44,7 +45,7 @@ mainForm.addEventListener('submit',(e)=>{
             <span class="category-badge">${inp2}</span>
         </div>
         <div class="actions">
-            <button class="btn btn-small btn-edit">Edit</button>
+            <button  class="btn btn-small btn-edit">Edit</button>
             <button class="btn btn-small btn-complete">Complete</button>
             <button class="btn btn-small btn-delete">Delete</button>
         </div>`;
@@ -64,7 +65,61 @@ mainForm.addEventListener('submit',(e)=>{
     card.setAttribute('data-status','completed') 
     updateStats()   
  }
+ if(e.target.classList.contains('btn-edit')){
+    const card = e.target.closest('.task-card');
+    const  titleSpan = card.querySelector('.task-title');
+    const input = document.createElement('input');
+    input.type ='text';
+    input.value = titleSpan.innerText;
+    input.className='edit-input';
+    titleSpan.replaceWith(input);
+    input.focus();
+    input.addEventListener('keydown',(e)=>{
+        if(e.key==='Enter'){
+        titleSpan.innerText = input.value;
+        input.replaceWith(titleSpan)
+        }
+    })
+ }
 })
+const gp = document.querySelector('#grandparent');
+const p = document.querySelector('#parent');
+const c = document.querySelector('#child');
+function showPropagation(p){
+    const box = document.createElement('div');
+    box.innerText = p;
+    box.style.color='red';
+    box.style.fontWeight='bold';
+    document.querySelector('#event-demo').appendChild(box);
+    setTimeout(()=>
+        box.remove(),2000)
+}
+gp.addEventListener('click',()=>{
+console.log('gradpatent:Bubbling');
+showPropagation('gradpatent:Bubbling')
+},false)
+p.addEventListener('click',()=>{
+console.log('patent:Bubbling');
+showPropagation('patent:Bubbling')
+
+},false)
+c.addEventListener('click',()=>{
+console.log('child:Bubbling');
+showPropagation('child:Bubbling')
+},false)
+gp.addEventListener('click',()=>{
+console.log('gradpatent:capcharing');
+showPropagation('gradpatent:capcharing')
+},true)
+p.addEventListener('click',()=>{
+console.log('patent:capcharing');
+showPropagation('patent:capcharing')
+},true)
+c.addEventListener('click',()=>{
+console.log('child:capcharing');
+showPropagation('child:capcharing')
+},true)
+
 
 
 
